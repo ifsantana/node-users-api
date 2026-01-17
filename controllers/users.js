4bac19916d90d7199cc88ca7b6c00c24e16bff3d
Italo <flaubert165@gmail.com>
2026-01-17 18:33:19 -0300

    add: different transaction types support

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const { User } = require('../models');
+const sequelize = require('../config/sequelize');
+const {Transaction} = require("sequelize");
 
 // var users = [
 //     { id: 1, name: 'John', email: 'test@gmail.com', age: 35 },
@@ -14,12 +16,11 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     let { id, name, email, age } = req.body;
-    // let newId = users.length + 1;
-    // in memory
-    // users.push({ id: , name, email, age });
-    let newUser = await User.create({ id, name, email, age })
-    console.log(newUser);
-    return res.status(201).json(newUser);
+
+    const user = await managedTransactionalUserCreation({ id, name, email, age });
+
+    console.log(user);
+    return res.status(201).json(user);
 });
 
 router.delete('/:id', async (req, res) => {
@@ -58,5 +59,60 @@ router.put('/:id', async (req, res) => {
         return res.status(409).json({})
     }
 })
+
+async function inMemoryUserCreation(req, res) {
+    // in memory
+
+    // let newId = users.length + 1;
+    // users.push({ id: , name, email, age });
+}
+
+async function nonTransactionalUserCreation(user) {
+    await User.create(user);
+}
+
+async function defaultUnmanagedTransactionalUserCreation(user) {
+    // unmanaged (not managed by sequelize) transaction with default isolation level (isolation level = db isolation level)
+    let t = await sequelize.transaction()
+
+    try {
+        let newUser = await User.create(user,  { transaction: t });
+        await t.commit();
+        return newUser;
+    } catch (error) {
+        console.error(error);
+        await t.rollback();
+    }
+}
+
+async function customUnmanagedTransactionalUserCreation(user) {
+    // unmanaged transaction custom isolation level
+    const t = await sequelize.transaction( { isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED } );
+
+    try {
+        let newUser = await User.create(user,  { transaction: t });
+        await t.commit();
+        return newUser;
+    } catch (error) {
+        console.error(error);
+        await t.rollback();
+    }
+}
+
+async function managedTransactionalUserCreation(user) {
+    // transaction managed by sequelize (locks, commits and rollbacks)
+    const result = await sequelize.transaction(async t => {
+        const newUser = await User.create(user, {transaction: t});
+
+        // If something goes wrong, we throw an error manually, so that Sequelize handles (rollback) everything automatically.
+        if(!newUser) {
+            throw new Error();
+        }
+
+        return newUser;
+    });
+
+    return result;
+}
 
 module.exports = router;
